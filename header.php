0ed6cb0074e2e3cd0045323e8ecf0051d9177921
groot314 <groot314@gmail.com>
2014-01-02 18:50:45 -0800

    Basic structure

---
+++ header.php
@@ -0,0 +1,1 @@
+<h1>SomeAwesome</h1>
---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>SomeAwesome.Net</title>
+</head>
+
+<?php include 'header.php'; ?>
+<nav><?php include 'nav.php'; ?>
+
+</body>
+</html>
