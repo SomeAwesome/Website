33d5889d31aee70788bb965d948fb65e8202c12b
groot314 <groot314@gmail.com>
2014-01-03 21:17:08 -0800

    added shop page

---
+++ shop/index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>SomeAwesome.Net</title>
+</head>
+
+<?php include 'header.php'; ?>
+<nav><?php include 'nav.php'; ?></nav>
+
+<iframe src="http://someawesome.buycraft.net" height="1000" width="100%" frameborder="0" scrolling="no"></iframe>
+
+</body>
+</html>
